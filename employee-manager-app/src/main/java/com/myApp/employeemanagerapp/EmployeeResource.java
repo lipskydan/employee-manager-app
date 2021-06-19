cd2a206a20bd2ff28ca11d6378f181b08788105d
lipskydan <lipsky@knu.ua>
2021-06-19 21:23:53 +0300

    Added main funcs to resource

--- employee-manager-app/src/main/java/com/myApp/employeemanagerapp/EmployeeResource.java
+++ employee-manager-app/src/main/java/com/myApp/employeemanagerapp/EmployeeResource.java
@@ -28,4 +28,22 @@ public class EmployeeResource {
         Employee employee = employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        Employee updateEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }

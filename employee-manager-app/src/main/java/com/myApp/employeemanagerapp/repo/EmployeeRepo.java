13482a4f303e55111a6a9d6f7ebe029a4538ece8
lipskydan <lipsky@knu.ua>
2021-06-19 20:45:32 +0300

    Added model, repo, service

---
+++ employee-manager-app/src/main/java/com/myApp/employeemanagerapp/repo/EmployeeRepo.java
@@ -0,0 +1,8 @@
+package com.myApp.employeemanagerapp.repo;
+
+import com.myApp.employeemanagerapp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+}
---
+++ employee-manager-app/src/main/java/com/myApp/employeemanagerapp/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.myApp.employeemanagerapp.service;
+
+import com.myApp.employeemanagerapp.model.Employee;
+import com.myApp.employeemanagerapp.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+}
